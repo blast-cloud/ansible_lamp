7ddda116d6e61b69c50465d985d94ef40848a8a1
elvistasie <elvistasie@gmail.com>
2023-04-30 22:20:47 +0100

    ecommerce script & playbook

---
+++ update_scripts/h_ecom_lib.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# Change directory to the repository
+cd ~/hasob/hasob-ecommerce-lib
+
+# Pull the latest changes from repository
+git pull
+
+# change directory 
+cd ~/hasob/
+
+# Set the input file or directory and output tarball name
+INPUT_DIR="~/hasob/hasob-ecommerce-lib"
+OUTPUT_TAR_DIR="ecommerce-lib.tar.gz"
+
+# Create the tar file
+tar -czf "$OUTPUT_TAR_DIR" "$INPUT_DIR"
+
+# Transfer to server
+# TERAS 
+scp -r "$OUTPUT_TAR_DIR" root@109.74.199.54:/var/www/
+# ITERUM
+scp -r "$OUTPUT_TAR_DIR" root@212.111.41.185:/var/www/
+# TETFUND staff
+scp -r "$OUTPUT_TAR_DIR" root@143.42.110.145:/var/www/
+# TETFUND beneficiary
+scp -r "$OUTPUT_TAR_DIR" root@178.79.148.204:/var/www/
+# TETFUND Impact & Remote Monitoring
+scp -r "$OUTPUT_TAR_DIR" root@212.71.244.231:/var/www/
+
+# change directory 
+cd ~/hasob/ansible_lamp/
+
+
+# Run the ansible playbook
+ansible-playbook unzip.yml -i inventory
+
+# change directory 
+cd ~/hasob/
+
+# Delete the tar file
+rm -r "$OUTPUT_TAR_DIR" 
