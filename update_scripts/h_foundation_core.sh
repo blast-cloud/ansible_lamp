10205de87f6aef050a78775f26466a5ffb4fef45
elvistasie <elvistasie@gmail.com>
2023-05-03 13:03:10 +0100

    edms

--- update_scripts/h_foundation_core.sh
+++ update_scripts/h_foundation_core.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Change directory to the repository
-cd ~/hasob/hasob-ecommerce-lib
+cd ~/hasob/hasob-foundation-core-bs-5
 
 # Pull the latest changes from repository
 git pull
@@ -10,8 +10,8 @@ git pull
 cd ~/hasob/
 
 # Set the input file or directory and output tarball name
-INPUT_DIR="./hasob-ecommerce-lib"
-OUTPUT_TAR_DIR="ecommerce-lib.tar.gz"
+INPUT_DIR="./hasob-foundation-core-bs-5 "
+OUTPUT_TAR_DIR="hasob-foundation-core-bs-5.tar.gz"
 
 # Create the tar file
 tar -czf "$OUTPUT_TAR_DIR" "$INPUT_DIR"
@@ -22,14 +22,14 @@ scp -r "$OUTPUT_TAR_DIR" root@109.74.199.54:/var/www/
 # ITERUM
 scp -r "$OUTPUT_TAR_DIR" root@212.111.41.185:/var/www/
 # TETFUND staff
-# scp -r "$OUTPUT_TAR_DIR" root@143.42.110.145:/var/www/
-# # TETFUND beneficiary
-# scp -r "$OUTPUT_TAR_DIR" root@178.79.148.204:/var/www/
-# # TETFUND Impact & Remote Monitoring
-# scp -r "$OUTPUT_TAR_DIR" root@212.71.244.231:/var/www/
+scp -r "$OUTPUT_TAR_DIR" root@143.42.110.145:/var/www/
+# TETFUND beneficiary
+scp -r "$OUTPUT_TAR_DIR" root@178.79.148.204:/var/www/
+# TETFUND Impact & Remote Monitoring
+scp -r "$OUTPUT_TAR_DIR" root@212.71.244.231:/var/www/
 
 # change directory 
-cd /etc/ansible/playbooks/ecom_lib
+cd /etc/ansible/playbooks/fc-bs-5
 
 
 # Run the ansible playbook
