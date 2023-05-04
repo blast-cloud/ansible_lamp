b24dffdac91110007a70eca3471e7ddcdaf616b1
blast-cloud <phwobo@gmail.com>
2023-05-04 11:58:53 +0100

    new-modules

--- update_scripts/h_foundation_core.sh
+++ update_scripts/h_foundation_core.sh
@@ -17,16 +17,23 @@ OUTPUT_TAR_DIR="hasob-foundation-core-bs-5.tar.gz"
 tar -czf "$OUTPUT_TAR_DIR" "$INPUT_DIR"
 
 # Transfer to server
-# TERAS 
+# TERAS
 scp -r "$OUTPUT_TAR_DIR" root@109.74.199.54:/var/www/
+
 # ITERUM
 scp -r "$OUTPUT_TAR_DIR" root@212.111.41.185:/var/www/
+
 # TETFUND staff
 scp -r "$OUTPUT_TAR_DIR" root@143.42.110.145:/var/www/
+
 # TETFUND beneficiary
 scp -r "$OUTPUT_TAR_DIR" root@178.79.148.204:/var/www/
+
 # TETFUND Impact & Remote Monitoring
 scp -r "$OUTPUT_TAR_DIR" root@212.71.244.231:/var/www/
+
+# QUICK-QUEUE
+scp -r "$OUTPUT_TAR_DIR" root@176.58.127.40:/var/www/
 
 # change directory 
 cd /etc/ansible/playbooks/fc-bs-5
